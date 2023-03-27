7d4cf9ad31eb56c97347cfa55ffbd3fbad253271
dev1week <rondo2860@gmail.com>
2023-03-27 20:54:14 +0900

    #1 -5 유저 도메인에 builder 추가

--- src/main/java/com/example/bank_service/User/User.java
+++ src/main/java/com/example/bank_service/User/User.java
@@ -1,5 +1,8 @@
 package com.example.bank_service.User;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -7,7 +10,8 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
-
+@Getter
+@NoArgsConstructor
 @EntityListeners(AuditingEntityListener.class)
 @Table(name = "user_tb")
 @Entity
@@ -38,4 +42,17 @@ public class User {
     @LastModifiedDate
     @Column(nullable = false)
     private LocalDateTime updatedAt;
+
+
+    @Builder
+    public User(Long id, String username, String password, String email, String fullname, UserEnum role, LocalDateTime createdAt, LocalDateTime updatedAt) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.fullname = fullname;
+        this.role = role;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
 }
