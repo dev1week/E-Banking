686118f4f6ca2b4547b1bdff5a158f6b14a5fdfc
dev1week <rondo2860@gmail.com>
2023-03-27 20:50:41 +0900

    #1 -3 유저 도메인 작성

---
+++ src/main/java/com/example/bank_service/User/User.java
@@ -0,0 +1,39 @@
+package com.example.bank_service.User;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+
+@Table(name = "user_tb")
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false, length = 20)
+    private String username;
+
+    @Column(nullable = false, length = 60)
+    private String password;
+
+    @Column(nullable = false, length = 20)
+    private String email;
+    @Column(nullable = false, length = 20)
+    private String fullname;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private UserEnum role;
+
+    @CreatedDate
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+}
