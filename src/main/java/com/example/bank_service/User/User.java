3fbfe488af7261c266d999750c99458659d35cc3
dev1week <rondo2860@gmail.com>
2023-03-27 20:52:30 +0900

    #1 -4 auditing 활성화
    user 도메인에서 사용하는 LocalDateTime을 자동 생성하기 위하여 BankServiceApplication과 user에 auditing 옵션을 설정하였다.

--- src/main/java/com/example/bank_service/User/User.java
+++ src/main/java/com/example/bank_service/User/User.java
@@ -2,11 +2,13 @@ package com.example.bank_service.User;
 
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
 
+@EntityListeners(AuditingEntityListener.class)
 @Table(name = "user_tb")
 @Entity
 public class User {
