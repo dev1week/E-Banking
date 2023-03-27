acdbde9d6b3441f6fd4c94cb9312fd065edafb94
dev1week <rondo2860@gmail.com>
2023-03-28 00:35:11 +0900

    hotfix2 - 안쓰는 import 정리

--- src/main/java/com/example/bank_service/Domain/Transaction/Transaction.java
+++ src/main/java/com/example/bank_service/Domain/Transaction/Transaction.java
@@ -4,17 +4,14 @@ package com.example.bank_service.Domain.Transaction;
 import java.time.LocalDateTime;
 
 import javax.persistence.Column;
-import javax.persistence.ConstraintMode;
 import javax.persistence.Entity;
 import javax.persistence.EntityListeners;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.FetchType;
-import javax.persistence.ForeignKey;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
