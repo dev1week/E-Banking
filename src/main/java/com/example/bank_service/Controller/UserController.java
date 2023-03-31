56dd57e32ecfb905424f09294686bfd86406b06e
dev1week <rondo2860@gmail.com>
2023-03-31 21:08:00 +0900

    #9 -8 user controller - 회원가입 유효성 검사 로직 추가
    - dto 멤버변수에 @NotEmpty 애너테이션으로 valid 체크 
    - user controller에서 @Valid 체크를 하여 에러를 반환할 경우 에러 dto를 직접 반환하는 로직 추가

---
+++ src/main/java/com/example/bank_service/Controller/UserController.java
@@ -0,0 +1,45 @@
+package com.example.bank_service.Controller;
+
+import com.example.bank_service.Dto.ResponseDto;
+import com.example.bank_service.Dto.User.UserReqDto.JoinReqDto;
+import com.example.bank_service.Dto.User.UserResDto.JoinResDto;
+import com.example.bank_service.Service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
+
+@RequiredArgsConstructor
+@RequestMapping("/api")
+@RestController
+public class UserController {
+
+    private final UserService userService;
+    @PostMapping("/join")
+   public ResponseEntity<?> mehtod(@RequestBody @Valid JoinReqDto joinReqDto, BindingResult bindingResult){
+
+
+        //유효성 검사 실패시 에러 dto 반환 로직
+        if(bindingResult.hasErrors()){
+            Map<String, String> errorMap = new HashMap<>();
+
+            for(FieldError error : bindingResult.getFieldErrors()){
+                errorMap.put(error.getField(), error.getDefaultMessage());
+            }
+            return new ResponseEntity<>(new ResponseDto<>(-1,"유효성 검사 실패",errorMap),HttpStatus.BAD_REQUEST);
+        }
+
+        JoinResDto joinResDto = userService.join(joinReqDto);
+        return new ResponseEntity<>(new ResponseDto<>(1,"회원가입 성공",joinResDto), HttpStatus.CREATED);
+   }
+
+}
--- src/main/java/com/example/bank_service/Dto/User/UserReqDto.java
+++ src/main/java/com/example/bank_service/Dto/User/UserReqDto.java
@@ -2,17 +2,23 @@ package com.example.bank_service.Dto.User;
 
 import com.example.bank_service.Domain.User.User;
 import com.example.bank_service.Domain.User.UserEnum;
-
 import lombok.Data;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import javax.validation.constraints.NotEmpty;
+
 public class UserReqDto {
+
     @Data
     public static class JoinReqDto{
         //to-do 유효성 검사해야함
+        @NotEmpty
         private String username;
+        @NotEmpty
         private String password;
+        @NotEmpty
         private String email;
+        @NotEmpty
         private String fullname;
 
         public User toEntity(BCryptPasswordEncoder passwordEncoder){
